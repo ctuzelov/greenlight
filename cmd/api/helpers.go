40bee55500abfa8e755c5cb84e10db434e86e39f
ctuzelov <ctuzelov@gmail.com>
2023-06-16 15:33:00 +0600

    reusable JSON encoder

---
+++ cmd/api/helpers.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil {
+		return 0, errors.New("invalid id parameter")
+	}
+	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	// why don't you use Encoder?
+	//When we call json.NewEncoder(w).Encode(data) the JSON is created and written to the
+	//http.ResponseWriter in a single step, which means there’s no opportunity to set HTTP
+	//response headers conditionally based on whether the Encode() method returns an error or
+	//not.
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// it's safe to add any headers that we want to include. We loop
+	// through the header map and add each header to the http.ResponseWriter header map.
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	js = append(js, '\n')
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	return nil
+}
