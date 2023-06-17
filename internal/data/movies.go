f6ffafec2932d4b4e3dba27173b87ecc6f6779bb
ctuzelov <ctuzelov@gmail.com>
2023-06-17 19:12:43 +0600

    omit zero values

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -8,8 +8,8 @@ type Movie struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
-	Year      int32     `json:"year"`
-	Runtime   Runtime   `json:"runtime,string"`
-	Genres    []string  `json:"genres"`
-	Version   int32     `json:"version"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   Runtime   `json:"runtime,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
+	Version   int32     `json:"version,omitempty"`
 }
